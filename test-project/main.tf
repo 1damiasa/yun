364df2dd586575ef57b556f0c7e872a84d90ee0c
Ubuntu <ubuntu@ip-172-31-8-67.us-east-2.compute.internal>
2025-02-05 12:48:18 +0000

    teraform-added

---
+++ test-project/main.tf
@@ -0,0 +1,58 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"  # Path to Minikube's kubeconfig file
+}
+
+resource "kubernetes_deployment" "hello_world" {
+  metadata {
+    name = "hello-world-app"
+    labels = {
+      app = "hello-world"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "hello-world"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "hello-world"
+        }
+      }
+
+      spec {
+        container {
+          name  = "hello-world"
+          image = "hello-world-app:latest"  # Using the locally built Docker image
+          port {
+            container_port = 5000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "hello_world" {
+  metadata {
+    name = "hello-world-service"
+  }
+
+  spec {
+    selector = {
+      app = "hello-world"
+    }
+
+    port {
+      port        = 80
+      target_port = 5000
+    }
+    type = "LoadBalancer"
+  }
+}
